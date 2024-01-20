a9e703eeabbdb62c5814ecb903b9a6198be7a11b
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-01-20 15:11:42 +0500

    connect carousel to api

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,14 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
-import { Image1, Image2, Image3, Image4, Image5 } from "../../assets";
+import { IMAGE_URL } from "../../constants";
+import { Image4 } from "../../assets";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 export default function Carousel() {
+  const [data, setData] = useState([]);
+
+  async function getData() {
+    await axios
+      .get("/banner")
+      .then((res) => setData(res?.data))
+      .catch(() => {
+        return;
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <section className="h-[calc(100vh-122px)] relative -top-[115px] mb-20 bg-sky-200">
+    <section className="h-[calc(100vh-122px)] relative -top-[115px] mb-20">
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -24,41 +42,24 @@ export default function Carousel() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide className="h-screen">
-          <img
-            src={Image1}
-            alt="carousel image"
-            className="w-full h-full object-cover object-center"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="h-screen">
-          <img
-            src={Image2}
-            alt="carousel image"
-            className="w-full h-full object-cover object-center"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="h-screen">
-          <img
-            src={Image3}
-            alt="carousel image"
-            className="w-full h-full object-cover object-center"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="h-screen">
-          <img
-            src={Image4}
-            alt="carousel image"
-            className="w-full h-full object-cover object-center"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="h-screen">
-          <img
-            src={Image5}
-            alt="carousel image"
-            className="w-full h-full object-cover object-center"
-          />
-        </SwiperSlide>
+        {data?.length === 0 && (
+          <SwiperSlide className="h-screen">
+            <img
+              src={Image4}
+              alt="artel logistics carousel image"
+              className="w-full h-full object-cover object-center"
+            />
+          </SwiperSlide>
+        )}
+        {data?.map((item, ind) => (
+          <SwiperSlide key={ind} className="h-screen">
+            <img
+              src={IMAGE_URL + item?.image}
+              alt="artel logistics carousel image"
+              className="w-full h-full object-cover object-center"
+            />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
