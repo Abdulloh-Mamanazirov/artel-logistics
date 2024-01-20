5ad09e7c692af485eaf3d4cbb133fd515f36fb92
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-01-20 12:36:01 +0500

    change uk flag to us flag

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,5 @@
 import Rus from "./rs-flag.gif";
-import Eng from "./uk-flag.gif";
+import Eng from "./us-flag.webp";
 import Uzb from "./uz-flag.gif";
 import Globe from "./globe.png";
 import Empty from "./empty.png";
